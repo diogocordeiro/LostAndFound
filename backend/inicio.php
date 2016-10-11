5b48d6db894cd7bc434beb338b3475bb7e717359
Paulinely Morgan <paulinelym@gmail.com>
2016-10-11 11:42:40 -0300

    integracao heroku5

---
+++ backend/inicio.php
@@ -0,0 +1,23 @@
+<? require('aRestrita.php'); ?>
+<? echo $_SESSION['Lost_Found']["nome"]."<br>" ?>
+<a href="logout.php">logout</a>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
+<p>oieeoeioeieoieoiedasfasdfasdfasdfasdf</p>
