1cc974b065265f1a1704e2f4142a5a3b32ba9be8
Paulinely Morgan <paulinelym@gmail.com>
2016-10-22 13:51:32 -0200

    Testes: us - cadastro usuario #3

--- backend/Testes/TestesUsuario.php
+++ backend/Testes/TestesUsuario.php
@@ -12,16 +12,22 @@ class TestesUsuario extends PHPUnit_Framework_TestCase{
 	private $mysqli;
 	private $dadosForm = ["teste567@teste567.com", "teste567", "teste567", "1995-01-10"];
 
+	//Funcao que prepara o ambiente
 	public function setUp(){
 
 		//Carrega os dados de bd do phpunit.xml
 		$this->mysqli = new mysqli($GLOBALS['dbHost'], $GLOBALS['dbUser'], $GLOBALS['dbPwd'], $GLOBALS['db']);
 
 		//Query para criar a tabela de usuarios
-		$tabelaUsuariosQuery = "create TABLE usuarios (id int(11) NOT NULL AUTO_INCREMENT,nome varchar(20) DEFAULT NULL,sobrenome varchar(30) DEFAULT NULL,email varchar(40) NOT NULL,senha varchar(32) NOT NULL,dNascimento date NOT NULL,situacao int(1) NOT NULL,PRIMARY KEY ( id ))";
+		$tabelaUsuariosQuery = "CREATE TABLE usuarios (id int(11) NOT NULL AUTO_INCREMENT,nome varchar(20) DEFAULT NULL,sobrenome varchar(30) DEFAULT NULL,email varchar(40) NOT NULL,senha varchar(32) NOT NULL,dNascimento date NOT NULL,situacao int(1) NOT NULL,PRIMARY KEY ( id ))";
 
 		//Cria tabela de usuarios
 		$this->mysqli->query($tabelaUsuariosQuery);
+	}
+
+	//Funcao para que a tabela seja eliminada pelo garbage collector
+	public function tearDown(){
+		$this->mysqli->query("DROP TABLE usuarios");
 	}
 
 	//Funcao para testar a insercao de uma novo usuario
