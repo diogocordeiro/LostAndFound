17ad2cc97a8e117ca5fdbc4176dc9f860ca94bec
Paulinely Morgan <paulinelym@gmail.com>
2016-10-22 12:06:54 -0200

    configuracao do ambiente de CI-Travis #5

--- backend/Usuario.php
+++ backend/Usuario.php
@@ -55,6 +55,54 @@ if (isset($_GET['tipo'])) {
 	echo "Defina o tipo do POST";
   }
 
+//Função para validar todos os campos passados nos formulários de cadastro
+function validarDadosCadastro($arrDados){
+
+	for ($i=0; $i < count($arrDados); $i++) { 
+		
+		//Valida contra XSS
+		$arrDados[$i] = validarString($arrDados[$i]);
+
+		//Data Nascimento
+		$anoNascimento = date('Y', strtotime($arrDados[3]));
+
+		//Senha
+		$mascaraPwd = '/[^a-z_\-0-9]/i';
+		//$containsLetter  = preg_match('/[a-zA-Z]/',    $string);
+		//$containsDigit   = preg_match('/\d/',          $string);
+
+		//Mascara para e-mail
+		$mascaraEmail = '/^(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){255,})(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){65,}@)(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22))(?:\\.(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-+[a-z0-9]+)*\\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-+[a-z0-9]+)*)|(?:\\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\\]))$/iD';
+
+		//Valida campos em branco
+		if ($i == 0 && strlen($arrDados[$i]) == 0) {
+			echo "Erro: e-mail não pode ficar vazio.";
+			exit;
+		} elseif (!(preg_match($mascaraEmail, $arrDados[0]) === 1)) {
+			echo "Erro: e-mail inválido.";
+			exit;
+		  } elseif ($i == 1 && strlen($arrDados[$i]) == 0) {
+				echo "Erro: senha não pode ficar vazia.";
+				exit;
+			  } elseif (strlen($arrDados[1]) < 8) {
+					echo "Erro: senha não pode ter menos de 8 dígitos.";
+					exit;
+			    } elseif (preg_match($mascaraPwd, $arrDados[1])) {
+					echo "Erro: senha inválida.";
+					exit;
+			    	} elseif ($arrDados[1] != $arrDados[2]) {
+				  		echo "Erro: as senhas devem ser iguais.";
+						exit;
+					    } elseif ($i == 3 && strlen($arrDados[$i]) == 0) {
+					  		echo "Erro: data de nascimento não pode ficar vazia.";
+							exit;
+					  	  } elseif ($anoNascimento > 1997) {
+					  			echo "Erro: Você precisa ser maior de 18 anos.";
+								exit;
+					  	    }
+	}//for
+}//validarDadosCadastro()
+
 //Função para checar a disponibilidade do e-mail
 function emailDisponivel($email){
 	
@@ -113,54 +161,6 @@ function incluirUsuario($myDb, $arrDados){
 	//Executa o statement
 	return $stmt->execute();
 }//function incluirUsuario()
-
-//Função para validar todos os campos passados nos formulários de cadastro
-function validarDadosCadastro($arrDados){
-
-	for ($i=0; $i < count($arrDados); $i++) { 
-		
-		//Valida contra XSS
-		$arrDados[$i] = validarString($arrDados[$i]);
-
-		//Data Nascimento
-		$anoNascimento = date('Y', strtotime($arrDados[3]));
-
-		//Senha
-		$mascaraPwd = '/[^a-z_\-0-9]/i';
-		//$containsLetter  = preg_match('/[a-zA-Z]/',    $string);
-		//$containsDigit   = preg_match('/\d/',          $string);
-
-		//Mascara para e-mail
-		$mascaraEmail = '/^(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){255,})(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){65,}@)(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22))(?:\\.(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-+[a-z0-9]+)*\\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-+[a-z0-9]+)*)|(?:\\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\\]))$/iD';
-
-		//Valida campos em branco
-		if ($i == 0 && strlen($arrDados[$i]) == 0) {
-			echo "Erro: e-mail não pode ficar vazio.";
-			exit;
-		} elseif (!(preg_match($mascaraEmail, $arrDados[0]) === 1)) {
-			echo "Erro: e-mail inválido.";
-			exit;
-		  } elseif ($i == 1 && strlen($arrDados[$i]) == 0) {
-				echo "Erro: senha não pode ficar vazia.";
-				exit;
-			  } elseif (strlen($arrDados[1]) < 8) {
-					echo "Erro: senha não pode ter menos de 8 dígitos.";
-					exit;
-			    } elseif (preg_match($mascaraPwd, $arrDados[1])) {
-					echo "Erro: senha inválida.";
-					exit;
-			    	} elseif ($arrDados[1] != $arrDados[2]) {
-				  		echo "Erro: as senhas devem ser iguais.";
-						exit;
-					    } elseif ($i == 3 && strlen($arrDados[$i]) == 0) {
-					  		echo "Erro: data de nascimento não pode ficar vazia.";
-							exit;
-					  	  } elseif ($anoNascimento > 1997) {
-					  			echo "Erro: Você precisa ser maior de 18 anos.";
-								exit;
-					  	    }
-	}//for
-}//validarDadosCadastro()
 
 //Método para alterar o usuário
 function alterarUsuario($myDb, $idUsuario, $arrDados){
