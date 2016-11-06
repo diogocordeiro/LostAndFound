a7b014c7f938312f8b6b5f9b0791082f961b3e00
Wagner de Lima <waglds@gmail.com>
2016-11-06 16:33:16 -0300

    [#130278667] us editar perfil: validacao completa.

--- backend/js/edicaoPerfil.js
+++ backend/js/edicaoPerfil.js
@@ -2,32 +2,59 @@ $('document').ready(function() {
 
     /*Validação de dados*/
     $("#edit-profile-form").validate({
-        // rules:
-        // {
-        //     email:
-        //     {
-        //         required: true,
-        //         email: true
-        //     },
-        //     senha: {
-        //         required: true
-        //     },
-        //     confirmaSenha: {
-        //         required: true,
-        //         equalTo: '#senha'
-        //     }
-        // },
-        // messages:
-        // {
-        //     email: "Por favor, entre com um e-mail válido.",
-        //     senha: {
-        //         required: "Por favor, forneça uma senha."
-        //     },
-        //     confirmaSenha: {
-        //         required: "Por favor, re-insira sua senha.",
-        //         equalTo: "As senhas não são iguais!"
-        //     }
-        // },
+        rules:
+        {
+            nome:
+            {
+                required: true,
+
+            },
+            sobrenome: {
+                required: true
+            },
+            sexo: {
+                required: true,
+            },
+            cidade: {
+                required: true
+            },
+
+            pais: {
+                required: true
+            },
+
+            celular: {
+                required: true,
+                minlength: 12
+            },
+
+            telefone: {
+                required: false,
+                minlength: 11
+            },
+
+            facebook: {
+                required: true
+            }
+        },
+        messages:
+        {
+            nome: "Por favor, entre com um nome válido.",
+            sobrenome: "Por favor, entre com sobrenome válido.",
+            sexo: "Por favor, escolha uma opção para sexo.",
+            cidade: "Por favor, informe uma cidade.",
+            pais: "Por favor, escolha um país.",
+            celular: {
+                required: "Por favor, informe um número de celular.",
+                minlength: "Por favor, informe um número de celular válido.",
+            },
+            telefone: {
+                minlength: "Por favor, informe um número de telefone válido.",
+            },
+            facebook: "Por favor, informe seu perfil no facebook."
+        },
+        errorElement: 'div',
+        errorLabelContainer: '#error-perfil-usuario',
         submitHandler: submitForm
     });
 
@@ -39,7 +66,6 @@ $('document').ready(function() {
         $.ajax({
             type: 'POST',
             url: "../backend/Usuario.php?tipo=edita",
-            //url: "../../testphp.php",
             data: dados,
             beforeSend: function () {
 
@@ -48,30 +74,20 @@ $('document').ready(function() {
             },
             success: function (data) {
 
-                // if (data == 1) {
-
-                //     $('#error').fadeIn(1000, function () {
-
-                //         $("#error").html('E-mail já existente!');
-                //         $("#btnSalvar").val('Cadastrar');
-                //     });
-                // }
-
                 if (data == "successo") {
 
                     $("#btnSalvar").val('Salvar');
-                    console.log("suceso");
                     //setTimeout('$("#edit-profile-form").fadeOut(500, function(){ $("#edit-profile-form").load("form-editar-perfil.php"); }); ', 5000);
                 }
 
-                // else {
+                else {
 
-                //     $('#error').fadeIn(1000, function () {
+                    $('#error').fadeIn(1000, function () {
 
-                //         $("#error").html('<div class=""><span class=""></span> &nbsp; '+data+' !</div>');
-                //         $("#btnSalvar").val('Salvar');
-                //     });
-                // }
+                        $("#error").html('<div class=""><span class=""></span> &nbsp; '+data+'</div>');
+                        $("#btnSalvar").val('Salvar');
+                    });
+                }
             },
             error: function (data) {
                 console.log(data);
@@ -81,26 +97,3 @@ $('document').ready(function() {
         return false;
     }
 });
-
-
-// $(document).ready(function() {
-//     $("#btnSalvar").click(function() {
-//
-//         var emailP = $("#email").val();
-//         var senhaP = $("#senha").val();
-//         var confirmaSenhaP = $("#confirmaSenha").val();
-//         var dNascimentoP = $("#dNascimento").val();
-//
-//         $.post("../backend/Usuario.php?tipo=novo", {email: emailP, senha: senhaP, confirmaSenha: confirmaSenhaP, dNascimento: dNascimentoP},
-//             function(response){
-//
-//                 // $("resultados").html("<div class=\"modal fade bs-example-modal-sm\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"mySmallModalLabel\" > <div class=\"modal-dialog modal-sm\" role=\"document\"> <div class=\"modal-content\">"+response+"</div> </div> </div>");
-//
-//                 $("body").html("<div class=\"center-block card mensagem-alerta\" id=\"alert\"> "+response+"<button onclick=\"Nova()\" class=\"btn btn-default btn-alerta\" type=\"button\" name=\"button\">Ok!</button> </div>");
-//
-//
-//                 // $(".resultados").html(response);
-//             }
-//             , "html");
-//     });
-// });
