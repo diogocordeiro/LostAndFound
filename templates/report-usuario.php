79959ca5e22c846474b24f5f0dbf731174c7769c
Diogo Cordeiro <diogo.ufrpe@gmail.com>
2016-12-06 16:18:11 -0300

    [130278853] - Comentarios

--- templates/report-usuario.php
+++ templates/report-usuario.php
@@ -222,6 +222,8 @@
                                         infowindow.open(map, marker);
                                     });
 
+
+
                                     infowindow.open(map, marker);
                                 }
                                 google.maps.event.addDomListener(window, 'load', init_map);
@@ -238,14 +240,26 @@
                       <div class="col-md-4 col-md-offset-4">
                         <br>
                     <hr>
-                        <p>
-                          OBS: AQUI VAI A JANELA Dos comentarios, Não uma imagem
-                        </p>
+                    <div id="disqus_thread"></div>
+<script>
 
-                        <!-- style="width: 250px;height:250px;border-radius:150px;-webkit-border-radius: 150px;-moz-border-radius: 150px" -->
+    /**
+     *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+     *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
 
-                        <img src="../static/img/comentario.png" class="img-rounded img-responsive img-raised">
+     var disqus_config = function () {
+     this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+     this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+     };
 
+    (function() { // DON'T EDIT BELOW THIS LINE
+        var d = document, s = d.createElement('script');
+        s.src = '//los-1.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
                       </div>
                     </div>
 
